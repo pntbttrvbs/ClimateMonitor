1bf85616e8e57c8a73ea896daf6324e5644ec59d
jaime <justin.spott@gmail.com>
2021-01-03 13:08:35 -0700

    changed board from 22 to 11, fixed typos in strings.

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import adafruit_dht
 import board
     
 app = Flask(__name__)
-sensor = adafruit_dht.DHT22(board.D4)
+sensor = adafruit_dht.DHT11(board.D4)
     
-@app.route(“/”)
+@app.route("/")
 def main():
    
    humidity = sensor.humidity
    temperature = sensor.temperature 
    templateData = {
-      ‘temperature’ : temperature,
-      ‘humidity’: humidity
+      'temperature' : temperature,
+      'humidity': humidity
    }
 
-   return render_template(‘main.html’, **templateData)
+   return render_template('main.html', **templateData)
      
-if __name__ == “__main__”:
-   app.run(host=’0.0.0.0′, port=80, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
