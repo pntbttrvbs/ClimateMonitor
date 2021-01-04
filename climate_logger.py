c0a6d28b5270439de6e4487202c8c8f685317ef0
jaime <justin.spott@gmail.com>
2021-01-03 23:00:53 -0700

    working climate logger; now need to do front end, and set up logger to run on start.

---
+++ climate_logger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+    
+import adafruit_dht
+import database
+import time
+
+pin = 4
+sensor = adafruit_dht.DHT11(pin)
+db = database.weather_database()
+
+def main():
+
+    humidity = sensor.humidity
+    temperature = sensor.temperature 
+    db.insert(temperature, 0, 0, 0, humidity, 0, 0, 0, 0)
+
+
+if __name__ == "__main__":
+    while True:
+        main()
+        time.sleep(60)
+
